fcb9bed6492835c11fe85fb8d08da17632ec2545
John Anderson <john@webkoils.com>
2019-08-16 16:32:29 -0400

    test commit

--- ios/RNSnapkit.m
+++ ios/RNSnapkit.m
@@ -9,5 +9,11 @@
 }
 RCT_EXPORT_MODULE()
 
+
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+{
+    RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
 @end
   
